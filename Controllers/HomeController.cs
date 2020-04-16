1c919035abc685ce80420541694f67ac22ffa7d5
Denny Chung <dennychung@Dennys-MacBook-Pro.local>
2020-04-16 12:21:42 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Form_Submission.Models;
+
+namespace Form_Submission.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [Route("success")]
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        [Route("success")]
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return View("Success");
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Form_Submission.Models
+{
+    public class User
+    {
+
+        [Required]
+        [MinLength(4)]
+        public string FirstName {get;set;}
+
+        [Required]
+        [MinLength(4)]
+        public string LastName {get;set;}
+
+        [Required]
+        [Range(1, 100)]
+        public int Age {get;set;}
+
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        public string Password {get;set;}
+
+    }
+}
